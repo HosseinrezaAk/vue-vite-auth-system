ca808d7b3075d791a8baa7ee035321f8c57ea3fb
Hosseinreza <hrakbari77@gmail.com>
2023-06-23 14:46:20 +0330

    change pinia to app

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,8 +4,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+const pinia = createPinia();
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 
 app.mount('#app')
