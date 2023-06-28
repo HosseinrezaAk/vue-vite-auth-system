3e4e9662f294b044b77aa49aca92b98f78e7778f
Hosseinreza <hrakbari77@gmail.com>
2023-06-28 14:54:58 +0330

    clear the whole app.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,17 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      
-    </div>
-  </header>
-
-  <main>
-    
-  </main>
 </template>
 
 <style scoped>
